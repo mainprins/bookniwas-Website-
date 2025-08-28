b87009bbcd2845f259f3d8cd8bf20fd6b1f7f181
Prince Bajgain <princebajagainprof@gmail.com>
2025-08-28 09:23:16 +0545

    third commit

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://bookniwas-website.onrender.com'],
     credentials: true,
 }));
 app.use(cookieParser());
